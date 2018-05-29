0ca482a354a8ddf59622f56e596f699d61aaade7
Keijiro Takahashi <keijiro@gmail.com>
2018-05-30 00:34:28 +0900

    enhancement

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -3,10 +3,15 @@ using Sensel;
 
 public class Test : MonoBehaviour
 {
+    [SerializeField] Shader _shader;
+
+    ComputeBuffer _buffer;
+    RenderTexture _texture1;
+    RenderTexture _texture2;
+    Material _material;
+
     SenselDevice _device;
     SenselSensorInfo _sensorInfo;
-    ComputeBuffer _buffer;
-    MaterialPropertyBlock _materialSheet;
 
     void OnEnable()
     {
@@ -30,20 +35,16 @@ public class Test : MonoBehaviour
         if (_device == null) return; // No device is available.
 
         if (_buffer == null)
-        {
-            _buffer = new ComputeBuffer(
-                _sensorInfo.num_cols * _sensorInfo.num_rows, sizeof(float)
-            );
+            _buffer = new ComputeBuffer(_sensorInfo.num_cols * _sensorInfo.num_rows, sizeof(float));
 
-            if (_materialSheet == null) _materialSheet = new MaterialPropertyBlock();
+        if (_texture1 == null)
+            _texture1 = new RenderTexture(1920, 1080, 0, RenderTextureFormat.ARGBHalf);
 
-            var renderer = GetComponent<Renderer>();
-            renderer.GetPropertyBlock(_materialSheet);
-            _materialSheet.SetBuffer("_ForceBuffer", _buffer);
-            _materialSheet.SetVector("_BufferDims",
-                new Vector2(_sensorInfo.num_cols, _sensorInfo.num_rows));
-            renderer.SetPropertyBlock(_materialSheet);
-        }
+        if (_texture2 == null)
+            _texture2 = new RenderTexture(1920, 1080, 0, RenderTextureFormat.ARGBHalf);
+
+        if (_material == null)
+            _material = new Material(_shader);
 
         _device.ReadSensor();
 
@@ -54,12 +55,24 @@ public class Test : MonoBehaviour
         var frame = _device.GetFrame();
 
         _buffer.SetData(frame.force_array);
+        _material.SetBuffer("_ForceBuffer", _buffer);
+        _material.SetVector("_BufferDims", new Vector2(_sensorInfo.num_cols, _sensorInfo.num_rows));
+        Graphics.Blit(_texture1, _texture2, _material);
+
+        var temp = _texture1;
+        _texture1 = _texture2;
+        _texture2 = temp;
     }
 
     void OnDisable()
     {
         if (_device != null) _device.StopScanning();
-        if (_buffer != null) _buffer.Dispose();
+
+        if (_buffer != null)
+        {
+            _buffer.Dispose();
+            _buffer = null;
+        }
     }
 
     void OnDestroy()
@@ -69,5 +82,14 @@ public class Test : MonoBehaviour
             _device.Close();
             _device = null;
         }
+
+        if (_texture1 != null) Destroy(_texture1);
+        if (_texture2 != null) Destroy(_texture2);
+        if (_material != null) Destroy(_material);
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(_texture1, destination);
     }
 }
---
+++ Assets/Update.shader
@@ -0,0 +1,67 @@
+﻿Shader "Sensel/Visualizer"
+{
+    Properties
+    {
+        _MainTex("", 2D) = ""
+    }
+    CGINCLUDE
+
+    #include "UnityCG.cginc"
+
+    sampler2D _MainTex;
+    float4 _MainTex_TexelSize;
+
+    Buffer<float> _ForceBuffer;
+    float2 _BufferDims;
+
+    half4 Fragment(v2f_img input) : SV_Target
+    {
+        float4 dd = float4(1, 1, -1, 0) * _MainTex_TexelSize.xyxy * 30;
+        half4 hp =
+            tex2D(_MainTex, input.uv - dd.xy) * 1 +
+            tex2D(_MainTex, input.uv - dd.wy) * 2 +
+            tex2D(_MainTex, input.uv - dd.zy) * 1 +
+            tex2D(_MainTex, input.uv + dd.zw) * 2 +
+            tex2D(_MainTex, input.uv        ) * 4 +
+            tex2D(_MainTex, input.uv + dd.xw) * 2 +
+            tex2D(_MainTex, input.uv + dd.zy) * 1 +
+            tex2D(_MainTex, input.uv + dd.wy) * 2 +
+            tex2D(_MainTex, input.uv + dd.xy) * 1;
+        hp *= 0.99 / 16;
+
+        float2 uv = input.uv;
+        uv.y = 1 - uv.y;
+        uv *= _BufferDims;
+        float2 iuv = (int2)uv;
+        float2 duv = uv - iuv;
+
+        int idx = iuv.y * _BufferDims.x + iuv.x;
+
+        float p00 = _ForceBuffer[idx];
+        float p01 = _ForceBuffer[idx + 1];
+        float p10 = _ForceBuffer[idx + _BufferDims.x];
+        float p11 = _ForceBuffer[idx + _BufferDims.x + 1];
+
+        float p = lerp(lerp(p00, p01, duv.x), lerp(p10, p11, duv.x), duv.y);
+        float p2 = saturate(max(hp.a, saturate(p * 0.05)));
+
+        float cr = sin(p2 * 8.73 + _Time.y * 5.31) * 0.5 + 0.5;
+        float cg = sin(p2 * 7.31 + _Time.y * 4.78) * 0.5 + 0.5;
+        float cb = sin(p2 * 5.11 + _Time.y * 3.53) * 0.5 + 0.5;
+
+        return half4(saturate(half3(cr, cg, cb) * saturate(p2 * 2)), p2);
+    }
+
+    ENDCG
+
+    SubShader
+    {
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert_img
+            #pragma fragment Fragment
+            ENDCG
+        }
+    }
+}
