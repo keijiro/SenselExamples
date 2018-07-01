51fc86c51b564fe29ea7414a539cf75c2c754501
Keijiro Takahashi <keijiro@gmail.com>
2018-07-01 18:28:26 +0900

    Add Fluid scene

---
+++ Assets/Test/Fluid/Fluid.cs
@@ -0,0 +1,237 @@
+﻿using UnityEngine;
+using Klak.Sensel;
+
+public class Fluid : MonoBehaviour
+{
+    #region Editable attributes
+
+    [SerializeField] Vector2Int _dimensions = new Vector2Int(512, 288);
+    [SerializeField] float _viscosity = 0.01f;
+    [SerializeField] float _force = 300;
+    [SerializeField] float _exponent = 200;
+
+    #endregion
+
+    #region Internal resources
+
+    [SerializeField, HideInInspector] ComputeShader _compute;
+    [SerializeField, HideInInspector] Shader _shader;
+
+    #endregion
+
+    #region Buffer objects
+
+    Material _shaderSheet;
+
+    static class Kernels
+    {
+        public const int Advect = 0;
+        public const int Force = 1;
+        public const int PSetup = 2;
+        public const int PFinish = 3;
+        public const int Jacobi1 = 4;
+        public const int Jacobi2 = 5;
+    }
+
+    int ThreadCountX { get { return _dimensions.x / 8; } }
+    int ThreadCountY { get { return _dimensions.y / 8; } }
+
+    // Vector field buffers
+    static class VFB
+    {
+        public static RenderTexture V1;
+        public static RenderTexture V2;
+        public static RenderTexture V3;
+        public static RenderTexture P1;
+        public static RenderTexture P2;
+    }
+
+    // Color buffers (for double buffering)
+    RenderTexture _colorRT1;
+    RenderTexture _colorRT2;
+
+    RenderTexture AllocateBuffer(int componentCount, int width = 0, int height = 0)
+    {
+        var format = RenderTextureFormat.ARGBHalf;
+        if (componentCount == 1) format = RenderTextureFormat.RHalf;
+        if (componentCount == 2) format = RenderTextureFormat.RGHalf;
+
+        if (width  == 0) width  = _dimensions.x;
+        if (height == 0) height = _dimensions.y;
+
+        var rt = new RenderTexture(width, height, 0, format);
+        rt.enableRandomWrite = true;
+        rt.Create();
+        return rt;
+    }
+
+    #endregion
+
+    #region Contact points
+
+    const int kMaxContacts = 16;
+
+    Contact [] _contacts = new Contact [kMaxContacts];
+    Vector4 [] _forceOrigins = new Vector4 [kMaxContacts];
+    Vector4 [] _forceVectors = new Vector4 [kMaxContacts];
+
+    #endregion
+
+    #region MonoBehaviour implementation
+
+    void OnValidate()
+    {
+        _dimensions = Vector2Int.Max(Vector2Int.one * 8, _dimensions);
+    }
+
+    void Start()
+    {
+        _shaderSheet = new Material(_shader);
+
+        VFB.V1 = AllocateBuffer(2);
+        VFB.V2 = AllocateBuffer(2);
+        VFB.V3 = AllocateBuffer(2);
+        VFB.P1 = AllocateBuffer(1);
+        VFB.P2 = AllocateBuffer(1);
+
+        _colorRT1 = AllocateBuffer(4, Screen.width, Screen.height);
+        _colorRT2 = AllocateBuffer(4, Screen.width, Screen.height);
+    }
+
+    void OnDestroy()
+    {
+        Destroy(_shaderSheet);
+
+        Destroy(VFB.V1);
+        Destroy(VFB.V2);
+        Destroy(VFB.V3);
+        Destroy(VFB.P1);
+        Destroy(VFB.P2);
+
+        Destroy(_colorRT1);
+        Destroy(_colorRT2);
+    }
+
+    Vector2 ConvertCoord(Contact c)
+    {
+        return new Vector2((c.X - 0.5f) * 16 / 9, c.Y - 0.5f);
+    }
+
+    void Update()
+    {
+        var dt = Time.deltaTime;
+        var dx = 1.0f / _dimensions.y;
+
+        // Update contact points.
+        for (var i = 0; i < _contacts.Length; i++)
+        {
+            var updated = TouchInput.GetContact(_contacts[i].ID);
+            if (_contacts[i].IsValid && updated.IsValid)
+            {
+                var pos = ConvertCoord(updated);
+                _forceOrigins[i] = pos;
+                _forceVectors[i] = (pos - ConvertCoord(_contacts[i])) * _force;
+            }
+            else
+            {
+                _forceOrigins[i] = Vector2.one * 1e+5f;
+                _forceVectors[i] = Vector2.zero;
+            }
+            _contacts[i] = updated;
+        }
+
+        // Append newly entered contact points.
+        foreach (var newContact in TouchInput.NewContacts)
+        {
+            for (var i = 0; i < _contacts.Length; i++)
+            {
+                if (!_contacts[i].IsValid)
+                {
+                    _contacts[i] = newContact;
+                    break;
+                }
+            }
+        }
+
+        // Common variables
+        _compute.SetFloat("Time", Time.time);
+        _compute.SetFloat("DeltaTime", dt);
+
+        // Advection
+        _compute.SetTexture(Kernels.Advect, "U_in", VFB.V1);
+        _compute.SetTexture(Kernels.Advect, "W_out", VFB.V2);
+        _compute.Dispatch(Kernels.Advect, ThreadCountX, ThreadCountY, 1);
+
+        // Diffuse setup
+        var dif_alpha = dx * dx / (_viscosity * dt);
+        _compute.SetFloat("Alpha", dif_alpha);
+        _compute.SetFloat("Beta", 4 + dif_alpha);
+        Graphics.CopyTexture(VFB.V2, VFB.V1);
+        _compute.SetTexture(Kernels.Jacobi2, "B2_in", VFB.V1);
+
+        // Jacobi iteration
+        for (var i = 0; i < 20; i++)
+        {
+            _compute.SetTexture(Kernels.Jacobi2, "X2_in", VFB.V2);
+            _compute.SetTexture(Kernels.Jacobi2, "X2_out", VFB.V3);
+            _compute.Dispatch(Kernels.Jacobi2, ThreadCountX, ThreadCountY, 1);
+
+            _compute.SetTexture(Kernels.Jacobi2, "X2_in", VFB.V3);
+            _compute.SetTexture(Kernels.Jacobi2, "X2_out", VFB.V2);
+            _compute.Dispatch(Kernels.Jacobi2, ThreadCountX, ThreadCountY, 1);
+        }
+
+        // Add external force
+        _compute.SetVectorArray("ForceOrigins", _forceOrigins);
+        _compute.SetVectorArray("ForceVectors", _forceVectors);
+        _compute.SetFloat("ForceExponent", _exponent);
+        _compute.SetTexture(Kernels.Force, "W_out", VFB.V2);
+        _compute.Dispatch(Kernels.Force, ThreadCountX, ThreadCountY, 1);
+
+        // Projection setup
+        _compute.SetTexture(Kernels.PSetup, "W_in", VFB.V2);
+        _compute.SetTexture(Kernels.PSetup, "DivW_out", VFB.V3);
+        _compute.SetTexture(Kernels.PSetup, "P_out", VFB.P1);
+        _compute.Dispatch(Kernels.PSetup, ThreadCountX, ThreadCountY, 1);
+
+        // Jacobi iteration
+        _compute.SetFloat("Alpha", -dx * dx);
+        _compute.SetFloat("Beta", 4);
+        _compute.SetTexture(Kernels.Jacobi1, "B1_in", VFB.V3);
+
+        for (var i = 0; i < 20; i++)
+        {
+            _compute.SetTexture(Kernels.Jacobi1, "X1_in", VFB.P1);
+            _compute.SetTexture(Kernels.Jacobi1, "X1_out", VFB.P2);
+            _compute.Dispatch(Kernels.Jacobi1, ThreadCountX, ThreadCountY, 1);
+
+            _compute.SetTexture(Kernels.Jacobi1, "X1_in", VFB.P2);
+            _compute.SetTexture(Kernels.Jacobi1, "X1_out", VFB.P1);
+            _compute.Dispatch(Kernels.Jacobi1, ThreadCountX, ThreadCountY, 1);
+        }
+
+        // Projection finish
+        _compute.SetTexture(Kernels.PFinish, "W_in", VFB.V2);
+        _compute.SetTexture(Kernels.PFinish, "P_in", VFB.P1);
+        _compute.SetTexture(Kernels.PFinish, "U_out", VFB.V1);
+        _compute.Dispatch(Kernels.PFinish, ThreadCountX, ThreadCountY, 1);
+
+        // Apply the velocity field to the color buffer.
+        _shaderSheet.SetVectorArray("_ForceOrigins", _forceOrigins);
+        _shaderSheet.SetFloat("_ForceExponent", _exponent);
+        _shaderSheet.SetTexture("_VelocityField", VFB.V1);
+        Graphics.Blit(_colorRT1, _colorRT2, _shaderSheet, 0);
+
+        // Swap the color buffers.
+        var temp = _colorRT1;
+        _colorRT1 = _colorRT2;
+        _colorRT2 = temp;
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(_colorRT1, destination, _shaderSheet, 1);
+    }
+
+    #endregion
+}
---
+++ Assets/Test/Fluid/Fluid.shader
@@ -0,0 +1,85 @@
+﻿// StableFluids - A GPU implementation of Jos Stam's Stable Fluids on Unity
+// https://github.com/keijiro/StableFluids
+
+Shader "Hidden/StableFluids"
+{
+    Properties
+    {
+        _MainTex("", 2D) = ""
+        _VelocityField("", 2D) = ""
+    }
+
+    CGINCLUDE
+
+    #define MAX_FORCES 16
+
+    #include "UnityCG.cginc"
+
+    sampler2D _MainTex;
+    float4 _MainTex_TexelSize;
+
+    sampler2D _VelocityField;
+
+    float2 _ForceOrigins[MAX_FORCES];
+    float _ForceExponent;
+
+    half4 frag_advect(v2f_img i) : SV_Target
+    {
+        // Time parameters
+        float time = _Time.y;
+        float deltaTime = unity_DeltaTime.x;
+
+        // Aspect ratio coefficients
+        float2 aspect = float2(_MainTex_TexelSize.y * _MainTex_TexelSize.z, 1);
+        float2 aspect_inv = float2(_MainTex_TexelSize.x * _MainTex_TexelSize.w, 1);
+
+        // Color advection with the velocity field
+        float2 delta = tex2D(_VelocityField, i.uv).xy * aspect_inv * deltaTime;
+        float3 color = tex2D(_MainTex, i.uv - delta).xyz;
+
+        // Dye (injection color)
+        float3 dye = saturate(sin(time * float3(2.72, 5.12, 4.98)) + 0.5);
+
+        for (uint idx = 0; idx < MAX_FORCES; idx++)
+        {
+            // Blend dye with the color from the buffer.
+            float2 pos = (i.uv - 0.5) * aspect;
+            float amp = exp(-_ForceExponent * distance(_ForceOrigins[idx], pos));
+            color = lerp(color, dye, saturate(amp * 100));
+        }
+
+        return half4(color, 1);
+    }
+
+    half4 frag_render(v2f_img i) : SV_Target
+    {
+        half3 rgb = tex2D(_MainTex, i.uv).rgb;
+
+        // Mixing channels up to get slowly changing false colors
+        //rgb = sin(float3(3.43, 4.43, 3.84) * rgb +
+        //          float3(0.12, 0.23, 0.44) * _Time.y) * 0.5 + 0.5;
+
+        return half4(GammaToLinearSpace(rgb), 1);
+    }
+
+    ENDCG
+
+    SubShader
+    {
+        Cull Off ZWrite Off ZTest Always
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert_img
+            #pragma fragment frag_advect
+            ENDCG
+        }
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert_img
+            #pragma fragment frag_render
+            ENDCG
+        }
+    }
+}
